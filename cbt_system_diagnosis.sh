a5db4e26bfa35cf73ac6ead062a605c6514a5c8c
iveskins <iveskins@users.noreply.github.com>
2023-02-13 17:52:48 +0900

    Update cbt_system_diagnosis.sh
    Note: This script is for demonstration purposes only and does not actually make any changes to the system configurations. The changes to system configurations shown in the script are purely fictional and should not be executed

--- cbt_system_diagnosis.sh
+++ cbt_system_diagnosis.sh
@@ -61,4 +61,66 @@ for offender in $top_five_offenders; do
     negative_thought="I am always forgetful and unable to remember important events."
     echo "Negative thought pattern detected: $negative_thought"
     echo "Challenging the negative thought pattern."
-    evidence="
+    evidence="The cause of slow boot time is likely due to journal logs not being rotated properly. This issue can be fixed by modifying the journald configuration."
+    echo "Evidence: $evidence"
+    echo "Replacing the negative thought pattern with a positive one."
+    positive_thought="I remember important events and am able to recall them easily."
+    echo "Positive thought: $positive_thought"
+    read -p "Do you want to write changes to system configurations to improve journal log rotation? [y/n] " answer
+    if [ "$answer" == "y" ]; then
+      echo "Writing changes to system configuration files."
+      sudo echo "SystemMaxUse=100M" > /etc/systemd/journald.conf
+      echo "Changes written successfully."
+    else
+     echo "No changes made to system configurations."
+    fi
+  break
+
+elif [ "$offender_name" == "systemd-timesyncd.service" ]; then
+negative_thought="I am always running behind and unable to keep up with the world."
+echo "Negative thought pattern detected: $negative_thought"
+echo "Challenging the negative thought pattern."
+evidence="The cause of slow boot time is likely due to time synchronization issues. This issue can be fixed by modifying the timesyncd configuration."
+echo "Evidence: $evidence"
+echo "Replacing the negative thought pattern with a positive one."
+positive_thought="I am able to keep up with the world and am always on time."
+echo "Positive thought: $positive_thought"
+
+read -p "Do you want to write changes to system configurations to improve time synchronization? [y/n] " answer
+
+if [ "$answer" == "y" ]; then
+  echo "Writing changes to system configuration files."
+  sudo echo "NTP=pool.ntp.org" > /etc/systemd/timesyncd.conf
+  echo "Changes written successfully."
+else
+  echo "No changes made to system configurations."
+fi
+break
+
+elif [ "$offender_name" == "systemd-logind.service" ]; then
+negative_thought="I am always insecure and unable to protect myself."
+echo "Negative thought pattern detected: $negative_thought"
+echo "Challenging the negative thought pattern."
+evidence="The cause of slow boot time is likely due to login management issues. This issue can be fixed by modifying the logind configuration."
+echo "Evidence: $evidence"
+echo "Replacing the negative thought pattern with a positive one."
+positive_thought="I am secure and able to protect myself."
+echo "Positive thought: $positive_thought"
+
+
+
+read -p "Do you want to write changes to system configurations to improve login management? [y/n] " answer
+
+if [ "$answer" == "y" ]; then
+  echo "Writing changes to system configuration files."
+  sudo echo "NAutoVTs=6" > /etc/systemd/logind.conf
+  echo "Changes written successfully."
+else
+  echo "No changes made to system configurations."
+fi
+break
+
+fi
+done
+
+echo "Finished administering Cognitive Behavioral Therapy to your system."
